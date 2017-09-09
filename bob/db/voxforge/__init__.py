11a56f7b02aeea8db8d2d8a3687e43eb88a0c056
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2017-09-09 17:58:51 +0200

    Removed import Client from bob.bio.base #10

--- bob/db/voxforge/__init__.py
+++ bob/db/voxforge/__init__.py
@@ -24,7 +24,7 @@ http://www.voxforge.org/
 
 from .query import Database
 import bob.io.audio
-from bob.bio.base.database.filelist.models import FileListFile, Client
+from bob.bio.base.database.filelist.models import FileListFile
 
 def get_config():
   """Returns a string containing the configuration information.
